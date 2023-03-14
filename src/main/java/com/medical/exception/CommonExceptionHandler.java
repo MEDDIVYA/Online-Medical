fe8a1f481893f2fcae2bc8b3406b8b0b7b39cfdc
MEDDIVYA <114343652+MEDDIVYA@users.noreply.github.com>
2023-03-14 19:00:44 +0530

    Update CommonExceptionHandler.java

--- src/main/java/com/medical/exception/CommonExceptionHandler.java
+++ src/main/java/com/medical/exception/CommonExceptionHandler.java
@@ -5,8 +5,6 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class CommonExceptionHandler {
