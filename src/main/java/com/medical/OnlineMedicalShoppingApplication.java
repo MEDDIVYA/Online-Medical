e881ffe28551a6fd6f74b0830190b00d7cc71df0
Divya <medam.divya@capgemini.com>
2023-03-14 16:58:56 +0530

    add all my files

--- src/main/java/com/medical/OnlineMedicalShoppingApplication.java
+++ src/main/java/com/medical/OnlineMedicalShoppingApplication.java
@@ -1,10 +1,13 @@
 package com.medical;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineMedicalShoppingApplication {
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineMedicalShoppingApplication.class, args);
